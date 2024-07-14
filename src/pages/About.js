0af9eee0e8627ca6e47b04fe8ecd2bfce81c176b
Saif Ali <sa594007@gmail.com>
2024-07-14 15:37:50 +0530

    Mohammas bold in font

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>Hii i am saif</div>
+    <div>
+      Hii i am <strong>Mohammad </strong>saif
+      </div>
   )
 }
 export default About;
