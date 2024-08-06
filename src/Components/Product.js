1aa3e9b9cc8bd1c5933b19c650920e550c9810f1
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-08-06 17:35:29 +0530

    adding the home product section ui

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,23 +1,63 @@
 import React from "react";
 import ProductCard from "./ProductCard";
+import headerDesign from "../assets/images/program-header-design.svg";
+import productImg1 from "../assets/images/product-img-1.png";
+import productImg2 from "../assets/images/product-img-2.png";
+import productImg3 from "../assets/images/product-img-3.png";
+import productImg4 from "../assets/images/product-img-4.png";
+
+import productCardIcon1 from '../assets/images/product-card-icon-1.svg'
+import productCardIcon2 from '../assets/images/product-card-icon-2.svg'
+import productCardIcon3 from '../assets/images/product-card-icon-3.svg'
+import productCardIcon4 from '../assets/images/product-card-icon-4.svg'
 
 const Product = () => {
   return (
-    <div>
-      <div>
-        <h1 className="text-center mt-12 text-3xl font-bold">Our Products</h1>
-        <div className="h-[2px] w-1/12 bg-red-600 mx-auto"></div>
-        <p className="mx-auto text-center text-xl font-bold mt-8 w-6/12">
-          Explore our wide range of products designed to meet your needs. Find
-          the perfect solution for you with just a few clicks.
-        </p>
-        <div className="flex justify-center align-center gap-[30px]">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+    <div className="w-full flex items-center justify-center flex-col">
+      <h1 className="text-center text-4xl md:text-5xl font-bold leading-none m-0 p-0">
+        Our Products
+      </h1>
+      <div className="flex justify-center items-center relative w-full m-0 p-0">
+        <img
+          src={headerDesign}
+          className="relative h-[50px] w-[50px]"
+          alt="header design"
+        />
+        <div className="relative top-[-10px]  h-[3px] w-[300px] bg-pink"></div>
+      </div>
+      <div className="h-[2px] w-1/12 bg-red-600 mx-auto"></div>
+      <p className="mx-auto text-center text-xl font-regular  mt-8 w-6/12">
+        Explore our wide range of products designed to meet your needs. Find the
+        perfect solution for you with just a few clicks.
+      </p>
+      <div className="flex justify-center align-center gap-[15px] w-10/12 mt-10">
+        <ProductCard
+          image={productImg1}
+          heading="Let's Read Educator's Handbook"
+          desc="Our Let's Read Educator's Handbook is your ultimate guide to teaching phonics skills effectively. Thank you for choosing Let's Read!"
+          icon={productCardIcon1}
+       />
+        <ProductCard
+          image={productImg2}
+          heading="Learner’s Guide 1"
+          desc="Our Let's Read Learner's Guide 1 is your ultimate guide to teaching phonics skills effectively. Thank you for choosing Let's Read!"
+          icon={productCardIcon2}
 
-        </div>
+        />
+        <ProductCard
+          image={productImg3}
+          heading="Learner’s Book 2: A Vibrant Journey into Phonics Sounds for Young Minds"
+          desc="Learner’s book 2 provides invaluable guidance in teaching phonics skills to young learners."
+          icon={productCardIcon3}
+
+        />
+        <ProductCard
+          image={productImg4}
+          heading="Learner's Guide 3"
+          desc="Discover Phonics Mastery with Learner's Guide Book 3"
+          icon={productCardIcon4}
+
+        />
       </div>
     </div>
   );
--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import Frame65 from "../assets/images/Frame 65.png";
-import Vector from "../assets/images/Vector.png";
-
-const ProductCard = () => {
+import React from "react";
+const ProductCard = ({image,heading,desc,icon}) => {
   return (
-    <div>
-        <div className='w-[250px] h-[520px]  mt-12 mb-12 rounded-xl overflow-hidden border-[2px] border-black'>
-                  <img
-                   src={Frame65}
-                   alt='frame'
-                   className='object-cover object-bottom'
-                   />
-                   <div className='w-[250]px h-[350px] bg-darkPurple rounded-t-3xl relative top-[-20px] '>
-                    <img
-                     src={Vector}
-                     alt='vector'
-                     className='w-[40px] h-[40px] mx-auto pt-4 object-center  '
-                     />
-                     <h1 className='mt-4 mx-2 text-white text-xl'>Let's Read Educator's Handbook</h1>
-                     <p className='mt-4 mx-2 text-white w-10/12'>Our Let's Read Educator's Handbook is your ultimate guide to teaching phonics skills effectively. Thank you for choosing Let's Read!</p>
-                     <button className="bg-yellow px-8 py-2 mr-10 rounded-lg font-bold mt-3 ml-12">
-                         Shop Now
-                        </button>
-                   </div>
-                </div>
+    <div className="w-3/12  rounded-xl overflow-hidden border-[1px] border-black m-0 p-0 relative bg-[#551F64]">
+      <img
+        src={image}
+        alt="frame"
+        className="object-cover object-bottom w-full"
+      />
+      <div className="w-full  bg-[#551F64] rounded-t-3xl relative top-[-20px] p-4">
+        <img
+          src={icon}
+          alt="vector"
+          className="w-[70px] h-[70px] mx-auto pt-4 object-center  "
+        />
+        <h1 className="mt-4 text-white text-xl">
+          {heading}
+        </h1>
+        <p className="mt-4 text-white text-justify">
+         {desc}
+        </p>
+        <button className="bg-yellow px-8 py-2 mr-10 rounded-lg font-bold mt-3 ml-12">
+          Shop Now
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
