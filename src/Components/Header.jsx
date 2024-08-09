1c8e67087e54e23857eeed0925961f2dcfe738fc
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-08-10 03:47:39 +0530

    fix the navlink active

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,27 +1,44 @@
-import React from 'react'
-import {NavLink} from "react-router-dom";
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import LetsReadImg from "../assets/images/image 2.png";
 
 const Header = () => {
+  const location = useLocation();
   return (
-        <nav className="bg-white p-2  ">
-            <div className='flex justify-between items-center'>
-             <img 
-              src={LetsReadImg}
-              alt='img'
-              className='w-[150px] h[40px]'
-             />
-            <nav className=' flex gap-6 items-center'>
-                <NavLink to="/" className="text-lg text-pink underline font-bold">Home</NavLink>
-                <NavLink to="/about" className="text-lg font-bold">About Us</NavLink>
-                <NavLink to="/products" className="text-lg font-bold">Products</NavLink>
-                <NavLink to="/programs" className="text-lg font-bold">Programs</NavLink>
-                <NavLink to="/resources" className="text-lg font-bold">Resources</NavLink>
-                <NavLink to="/faq" className="text-lg font-bold">FAQ</NavLink>
-                <button className=' px-4 py-2 ml-12  mx-6 rounded-lg font-bold' style={{background:'linear-gradient(60deg, rgba(255,226,89,1) 0%, rgba(255,167,81,1) 100%)'}}>Enquire Now</button>
-            </nav>
-            </div> 
+    <nav className="bg-white p-2  ">
+      <div className="flex justify-between items-center">
+        <img src={LetsReadImg} alt="img" className="w-[150px] h[40px]" />
+        <nav className=" flex gap-6 items-center">
+          <NavLink to="/" className={`text-lg font-bold ${location.pathname==='/'?'text-pink underline':''}`}>
+            Home
+          </NavLink>
+          <NavLink to="/about" className={`text-lg font-bold ${location.pathname==='/about'?'text-pink underline':''}`}>
+            About Us
+          </NavLink>
+          <NavLink to="/products" className={`text-lg font-bold ${location.pathname==='/products'?'text-pink underline':''}`}>
+            Products
+          </NavLink>
+          <NavLink to="/programs" className={`text-lg font-bold ${location.pathname==='/programs'?'text-pink underline':''}`}>
+            Programs
+          </NavLink>
+          <NavLink to="/resources" className={`text-lg font-bold ${location.pathname==='/resources'?'text-pink underline':''}`}>
+            Resources
+          </NavLink>
+          <NavLink to="/faq" className={`text-lg font-bold ${location.pathname==='/faq'?'text-pink underline':''}`}>
+            FAQ
+          </NavLink>
+          <button
+            className=" px-4 py-2 ml-12  mx-6 rounded-lg font-bold"
+            style={{
+              background:
+                "linear-gradient(60deg, rgba(255,226,89,1) 0%, rgba(255,167,81,1) 100%)",
+            }}
+          >
+            Enquire Now
+          </button>
         </nav>
-  )
-}
+      </div>
+    </nav>
+  );
+};
 export default Header;
