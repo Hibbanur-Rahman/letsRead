565decf51da93c2ad71d1198c099c372ebbcdacb
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-08-11 00:00:12 +0530

    adding the product page ui

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -6,12 +6,12 @@ import productImg2 from "../assets/images/product-img-2.png";
 import productImg3 from "../assets/images/product-img-3.png";
 import productImg4 from "../assets/images/product-img-4.png";
 
-import productCardIcon1 from '../assets/images/product-card-icon-1.svg'
-import productCardIcon2 from '../assets/images/product-card-icon-2.svg'
-import productCardIcon3 from '../assets/images/product-card-icon-3.svg'
-import productCardIcon4 from '../assets/images/product-card-icon-4.svg'
+import productCardIcon1 from "../assets/images/product-card-icon-1.svg";
+import productCardIcon2 from "../assets/images/product-card-icon-2.svg";
+import productCardIcon3 from "../assets/images/product-card-icon-3.svg";
+import productCardIcon4 from "../assets/images/product-card-icon-4.svg";
 
-import productSideDesign from '../assets/images/products-side-bottom-design.svg'
+import productSideDesign from "../assets/images/products-side-bottom-design.svg";
 
 const Product = () => {
   return (
@@ -32,37 +32,34 @@ const Product = () => {
         Explore our wide range of products designed to meet your needs. Find the
         perfect solution for you with just a few clicks.
       </p>
-     
+
       <div className="flex justify-center align-center gap-[15px] w-10/12 mt-10 relative">
-      <div className="absolute right-[-130px] top-[-100px] rotate-180 -z-10">
-        <img src={productSideDesign} alt="" className="w-full h-full" />
-      </div>
+        <div className="absolute right-[-130px] top-[-100px] rotate-180 -z-10">
+          <img src={productSideDesign} alt="" className="w-full h-full" />
+        </div>
         <ProductCard
           image={productImg1}
           heading="Let's Read Educator's Handbook"
           desc="Our Let's Read Educator's Handbook is your ultimate guide to teaching phonics skills effectively. Thank you for choosing Let's Read!"
           icon={productCardIcon1}
-       />
+        />
         <ProductCard
           image={productImg2}
           heading="Learner’s Guide 1"
           desc="Our Let's Read Learner's Guide 1 is your ultimate guide to teaching phonics skills effectively. Thank you for choosing Let's Read!"
           icon={productCardIcon2}
-
         />
         <ProductCard
           image={productImg3}
           heading="Learner’s Book 2: A Vibrant Journey into Phonics Sounds for Young Minds"
           desc="Learner’s book 2 provides invaluable guidance in teaching phonics skills to young learners."
           icon={productCardIcon3}
-
         />
         <ProductCard
           image={productImg4}
           heading="Learner's Guide 3"
           desc="Discover Phonics Mastery with Learner's Guide Book 3"
           icon={productCardIcon4}
-
         />
       </div>
       <div className="absolute left-[-30px] bottom-[-70px] h-[200px] w-[200px] -z-10">
--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import FaqImg from "../assets/images/faq.png";
-import FaqImg2 from "../assets/images/faq2.png";
-import AccordionUse from "../Components/AccordianUsage";
+// import FaqImg2 from "../assets/images/faq2.png";
+// import AccordionUse from "../Components/AccordianUsage";
 
+import frequentlyAskedImg from "../assets/images/frequently-asked-question-img.png";
+
+import productSideDesign from "../assets/images/products-side-bottom-design.svg";
+
+import yellowStar from "../assets/images/yellow-star.svg";
 
 const FAQ = () => {
+  const [isFaqShow1, setIsFaqShow1] = useState(false);
+  const [isFaqShow2, setIsFaqShow2] = useState(false);
+  const [isFaqShow3, setIsFaqShow3] = useState(false);
+  const [isFaqShow4, setIsFaqShow4] = useState(false);
+  const [isFaqShow5, setIsFaqShow5] = useState(false);
+  const [isFaqShow6, setIsFaqShow6] = useState(false);
   return (
     <div>
       <div>
@@ -21,14 +32,195 @@ const FAQ = () => {
         </div>
         <img src={FaqImg} alt="faq" />
       </div>
-        <div className="mt-24 mb-24 flex gap-[60px]" >
-          <img 
-          src={FaqImg2}    
-          alt="img"
-          className="h-[460px] w-[400px]"
-          />
+      {/* <div className="mt-24 mb-24 flex gap-[60px]">
+        <img src={FaqImg2} alt="img" className="h-[460px] w-[400px]" />
         <AccordionUse />
+      </div> */}
+
+      <div className="w-full flex flex-col items-center justify-center mt-16 relative">
+
+        <div className="w-11/12 flex justify-between relative mt-16">
+          <div className="absolute left-[-70px] top-[-70px] h-[200px] w-[200px] -z-10">
+            <img src={productSideDesign} alt="" className="w-full h-full" />
+          </div>
+          <div className="w-4/12 p-8">
+            <img
+              src={frequentlyAskedImg}
+              alt=""
+              className="rounded-3xl h-[600px] w-full"
+            />
+          </div>
+          <div className="w-7/12">
+            <div className="faq-item w-full my-4">
+              <div
+                className={`faq-question shadow-xl transition-all p-4 cursor-pointer  border-[1px] border-[#969696] flex items-center justify-between ${
+                  isFaqShow1
+                    ? "rounded-3xl rounded-tr-none rounded-bl-none bg-darkPurple text-white font-bold"
+                    : "rounded-xl"
+                }`}
+                onClick={() => setIsFaqShow1(!isFaqShow1)}
+              >
+                <p className="text-xl">
+                  1. How does Let&apos;s Read teach reading skills?
+                </p>
+                <i className="bi bi-chevron-down text-2xl font-bold"></i>
+              </div>
+              <div
+                className={`faq-answer  items-center p-2 border-[1px] rounded-lg  mt-3 shadow-lg ${
+                  isFaqShow1 ? "flex" : "hidden"
+                }`}
+              >
+                <p className="text-xl">
+                  {" "}
+                  Let&apos;s Read adopts a holistic method, integrating engaging
+                  materials and interactive activities for a comprehensive
+                  learning experience.
+                </p>
+              </div>
+            </div>
+            <div className="faq-item w-full my-4">
+              <div
+                className={`faq-question shadow-xl transition-all p-4 cursor-pointer  border-[1px] border-[#969696] flex items-center justify-between ${
+                  isFaqShow2
+                    ? "rounded-3xl rounded-tr-none rounded-bl-none bg-darkPurple text-white font-bold"
+                    : "rounded-xl"
+                }`}
+                onClick={() => setIsFaqShow2(!isFaqShow2)}
+              >
+                <p className="text-xl">
+                  2. How Can I track my child&apos;s progress with Let&apos;s
+                  Read?
+                </p>
+                <i className="bi bi-chevron-down text-2xl font-bold"></i>
+              </div>
+              <div
+                className={`faq-answer  items-center p-2 border-[1px] rounded-lg  mt-3 shadow-lg ${
+                  isFaqShow2 ? "flex" : "hidden"
+                }`}
+              >
+                <p className="text-xl">
+                  {" "}
+                  Let&apos;s Read adopts a holistic method, integrating engaging
+                  materials and interactive activities for a comprehensive
+                  learning experience.
+                </p>
+              </div>
+            </div>
+            <div className="faq-item w-full my-4">
+              <div
+                className={`faq-question shadow-xl transition-all p-4 cursor-pointer  border-[1px] border-[#969696] flex items-center justify-between ${
+                  isFaqShow3
+                    ? "rounded-3xl rounded-tr-none rounded-bl-none bg-darkPurple text-white font-bold"
+                    : "rounded-xl"
+                }`}
+                onClick={() => setIsFaqShow3(!isFaqShow3)}
+              >
+                <p className="text-xl">
+                  3. Is Let&apos;s Read suitable for children of all ages?
+                </p>
+                <i className="bi bi-chevron-down text-2xl font-bold"></i>
+              </div>
+              <div
+                className={`faq-answer  items-center p-2 border-[1px] rounded-lg  mt-3 shadow-lg ${
+                  isFaqShow3 ? "flex" : "hidden"
+                }`}
+              >
+                <p className="text-xl">
+                  {" "}
+                  Let&apos;s Read adopts a holistic method, integrating engaging
+                  materials and interactive activities for a comprehensive
+                  learning experience.
+                </p>
+              </div>
+            </div>
+            <div className="faq-item w-full my-4">
+              <div
+                className={`faq-question shadow-xl transition-all p-4 cursor-pointer  border-[1px] border-[#969696] flex items-center justify-between ${
+                  isFaqShow4
+                    ? "rounded-3xl rounded-tr-none rounded-bl-none bg-darkPurple text-white font-bold"
+                    : "rounded-xl"
+                }`}
+                onClick={() => setIsFaqShow4(!isFaqShow4)}
+              >
+                <p className="text-xl">
+                  4. How Can I get started with Let&apos;s Read at home?
+                </p>
+                <i className="bi bi-chevron-down text-2xl font-bold"></i>
+              </div>
+              <div
+                className={`faq-answer  items-center p-2 border-[1px] rounded-lg  mt-3 shadow-lg ${
+                  isFaqShow4 ? "flex" : "hidden"
+                }`}
+              >
+                <p className="text-xl">
+                  {" "}
+                  Let&apos;s Read adopts a holistic method, integrating engaging
+                  materials and interactive activities for a comprehensive
+                  learning experience.
+                </p>
+              </div>
+            </div>
+            <div className="faq-item w-full my-4">
+              <div
+                className={`faq-question shadow-xl transition-all p-4 cursor-pointer  border-[1px] border-[#969696] flex items-center justify-between ${
+                  isFaqShow5
+                    ? "rounded-3xl rounded-tr-none rounded-bl-none bg-darkPurple text-white font-bold"
+                    : "rounded-xl"
+                }`}
+                onClick={() => setIsFaqShow5(!isFaqShow5)}
+              >
+                <p className="text-xl">
+                  15 HAre there any teacher training resources available?
+                </p>
+                <i className="bi bi-chevron-down text-2xl font-bold"></i>
+              </div>
+              <div
+                className={`faq-answer  items-center p-2 border-[1px] rounded-lg  mt-3 shadow-lg ${
+                  isFaqShow5 ? "flex" : "hidden"
+                }`}
+              >
+                <p className="text-xl">
+                  {" "}
+                  Let&apos;s Read adopts a holistic method, integrating engaging
+                  materials and interactive activities for a comprehensive
+                  learning experience.
+                </p>
+              </div>
+            </div>
+            <div className="faq-item w-full my-4">
+              <div
+                className={`faq-question shadow-xl transition-all p-4 cursor-pointer  border-[1px] border-[#969696] flex items-center justify-between ${
+                  isFaqShow6
+                    ? "rounded-3xl rounded-tr-none rounded-bl-none bg-darkPurple text-white font-bold"
+                    : "rounded-xl"
+                }`}
+                onClick={() => setIsFaqShow6(!isFaqShow6)}
+              >
+                <p className="text-xl">
+                  6.Is Let&apos;s Read suitable for non-English setting?
+                </p>
+                <i className="bi bi-chevron-down text-2xl font-bold"></i>
+              </div>
+              <div
+                className={`faq-answer  items-center p-2 border-[1px] rounded-lg  mt-3 shadow-lg ${
+                  isFaqShow6 ? "flex" : "hidden"
+                }`}
+              >
+                <p className="text-xl">
+                  {" "}
+                  Let&apos;s Read adopts a holistic method, integrating engaging
+                  materials and interactive activities for a comprehensive
+                  learning experience.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className="absolute bottom-[-50px] right-[-50px] h-[150px] w-[150px]">
+          <img src={yellowStar} alt="star" className="h-full w-full" />
+        </div>
+      </div>
+      
     </div>
   );
 };
